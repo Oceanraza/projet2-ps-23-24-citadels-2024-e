75a3df2332548457837d1965966f54e388ccf728
QuentinELLEON <quentin.elleon@etu.unice.fr>
2023-12-04 16:07:56 +0100

    hotfix, changed Arraylist to list for complexity

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -3,7 +3,7 @@ package fr.cotedazur.univ.polytech.startingpoint;
 public class Main {
     //if a player has 8 districts built, he wins
     public static boolean isFinished(Player player) {
-        return player.getDistrictsBuilt().size() == 8;
+        return player.getDistrictsBuilt().length == 8;
     }
 
     public static void main(String... args) {
