f567a86b5273951834f94fe31e3fdd80a0b7a6a2
QuentinELLEON <quentin.elleon@etu.unice.fr>
2023-12-04 16:57:35 +0100

    [feature] Initialisation du deck2

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -1,12 +1,17 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
+import java.util.ArrayList;
+
 public class Main {
     //if a player has 8 districts built, he wins
+
     public static boolean isFinished(Player player) {
         return player.getDistrictsBuilt().size() == 8;
     }
 
     public static void main(String... args) {
+        Game newGame = new Game();
+        System.out.println(newGame);
         isFinished(new Player() {
         });
 
