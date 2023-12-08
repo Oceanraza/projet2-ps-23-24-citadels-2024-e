6309db96dc723affa93c0cc7542a4f537031e3f4
Oceanraza <147170631+Oceanraza@users.noreply.github.com>
2023-12-08 15:06:49 +0100

    add MainTest

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/MainTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/MainTest.java
@@ -3,9 +3,16 @@ package fr.cotedazur.univ.polytech.startingpoint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     Player player;
 
     @BeforeEach
@@ -24,7 +31,38 @@ class MainTest {
     }
 
     @Test
-    void testIsWinner() {
+    void testCalculateScores() {
+        Player firstBuilder = new Bot();
+        Player secondPlayer = new Bot();
+        List<Player> players = Arrays.asList(firstBuilder, secondPlayer);
 
+        for (int i = 0; i < 8; i++) {
+            firstBuilder.getDistrictsBuilt().add(new District("test", i, DistrictColor.marchand));
+            secondPlayer.getDistrictsBuilt().add(new District("test", i, DistrictColor.marchand));
+        }
+
+        List<Player> scoredPlayers = Main.calculateScores(players, firstBuilder);
+
+        assertEquals(36, scoredPlayers.get(0).score);
+        assertEquals(34, scoredPlayers.get(1).score);
+    }
+    @Test
+    void testAnnounceWinner() {
+        Player firstBuilder = new Bot("Player 1");
+        Player secondPlayer = new Bot("Player 2");
+        List<Player> players = Arrays.asList(firstBuilder, secondPlayer);
+
+        for (int i = 0; i < 8; i++) {
+            firstBuilder.getDistrictsBuilt().add(new District("test", i, DistrictColor.marchand)); // 34 points
+            secondPlayer.getDistrictsBuilt().add(new District("test", i, DistrictColor.marchand)); // 32 points
+        }
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Main.announceWinner(players, firstBuilder);
+
+        String expectedOutput = "Player 1 : 34 points"+ LINE_SEPARATOR+ "Player 2 : 32 points"+ LINE_SEPARATOR+ "Player 1 gagne la partie avec 34 points !"+ LINE_SEPARATOR;
+        assertEquals(expectedOutput, outContent.toString());
     }
 }
