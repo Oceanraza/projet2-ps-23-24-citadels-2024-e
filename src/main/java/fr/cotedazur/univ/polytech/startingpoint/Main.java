c894375d16ab9715d2655e0e75a42b7fc1d1df8b
Mathis-Jullien <mathis.jullien@etu.univ-cotedazur.fr>
2023-12-08 01:29:43 +0100

    feat : add isWinner method

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -1,5 +1,7 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
@@ -8,6 +10,32 @@ public class Main {
     // If a player has 8 districts built, he wins
     public static boolean isFinished(Player player) {
         return player.getDistrictsBuilt().size() == 8;
+    }
+
+    public static Player isWinner(List<Player> players, Player firstBuilder) {
+        int maxScore = 0;
+        Player winner = players.get(0);
+        for(Player player : players) {
+            int score = player.gold;
+            ArrayList<DistrictColor> districtColors = new ArrayList<>();
+            for (District district : player.getDistrictsBuilt()) {
+                score += district.getPrice();
+                districtColors.add(district.getColor());
+            }
+            if (districtColors.size() == DistrictColor.values().length) {
+                score += 3;
+            }
+            if (player == firstBuilder) {
+                score += 4;
+            } else if (isFinished(player)) {
+                score += 2;
+            }
+            if (score > maxScore) {
+                maxScore = score;
+                winner = player;
+            }
+        }
+        return winner;
     }
 
     public static void main(String... args) {
