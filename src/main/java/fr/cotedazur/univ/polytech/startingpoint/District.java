c9ec9040a3ad265ff2a8e81ab93fd5b91d90213f
Darina <147172390+DarinaChan@users.noreply.github.com>
2023-12-25 21:12:45 +0100

    Remove record for District class
    Record can use heritage which will be problematic for bonus cards (cartes merveilles)

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/District.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/District.java
@@ -1,7 +1,24 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
-public record District(String name, int price, DistrictColor color) {
-
+public class District {
+    private DistrictColor color;
+    private int price;
+    private String name;
+    
+    public District(String name, int price, DistrictColor color){
+        this.color = color; this.price = price; this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public DistrictColor getColor() {
+        return color;
+    }
+    
     public String toString() {
         return name + "-" + price + "-" + color;
     }
