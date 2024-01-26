20b3a434aef008e8904e2d79cec7f2e07a670e3a
Oceanraza <147170631+Oceanraza@users.noreply.github.com>
2024-01-26 16:06:03 +0100

    :memo: Adding JavDoc

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/board/Deck.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/board/Deck.java
@@ -6,6 +6,10 @@ import fr.cotedazur.univ.polytech.startingpoint.city.District;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Deck
+ */
+
 public class Deck {
     private ArrayList<District> cards;
     private ArrayList<District> discardPile;
@@ -29,6 +33,11 @@ public class Deck {
         return str.toString();
     }
 
+    /**
+     * Adds a district to the deck.
+     *
+     * @param district the district to be added
+     */
 
     public void addDistrict(District district) {
         if (district != null) {
@@ -36,10 +45,20 @@ public class Deck {
         }
     }
 
+    /**
+     * Shuffles the deck.
+     */
+
     public void shuffle() {
         Collections.shuffle(this.cards);
     }
 
+    /**
+     * Draws a card from the deck.
+     *
+     * @return the card drawn
+     * @throws EmptyDeckException if the deck is empty
+     */
     public District drawCard() {
         if (this.isEmpty()) {
             throw new EmptyDeckException("Deck is empty");
@@ -47,20 +66,35 @@ public class Deck {
         return this.cards.remove(this.size() - 1);
     }
 
+    /**
+     * Checks if the deck is empty.
+     *
+     * @return true if the deck is empty, false otherwise
+     */
+
     public boolean isEmpty() {
         return this.cards.isEmpty();
     }
+
+    /**
+     * Returns the number of cards in the deck.
+     *
+     * @return the number of cards in the deck
+     */
 
     public int size() {
         return this.cards.size();
     }
 
-
+    /**
+     * Discards a district.
+     *
+     * @param district the district to be discarded
+     */
     public void discardDistrict(District district) {
         this.cards.remove(district);
         this.discardPile.add(district);
     }
-
 
     public ArrayList<District> getCards() {
         return this.cards;
