05df44d1ffe829c7a28e2c468ee7cd76c3c180b6
DarinaChan <darina.chan@etu.unice.fr>
2024-01-26 16:14:25 +0100

    Add assassin

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/character/Assassin.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/character/Assassin.java
@@ -1,2 +1,16 @@
-package fr.cotedazur.univ.polytech.startingpoint.character;public class Assassin {
+package fr.cotedazur.univ.polytech.startingpoint.character;
+
+import fr.cotedazur.univ.polytech.startingpoint.Game;
+import fr.cotedazur.univ.polytech.startingpoint.city.DistrictColor;
+import fr.cotedazur.univ.polytech.startingpoint.player.Player;
+
+public class Assassin extends GameCharacter {
+    public Assassin () {
+        super("Assassin", 1);
+    }
+
+    @Override
+    public void specialEffect(Player player, Game game, Object... optionalArgs) {
+        GameCharacter targetedCharacter = (GameCharacter) optionalArgs[0];
+    }
 }
